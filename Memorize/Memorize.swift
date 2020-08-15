fcfa0aa4b684e874462aab4762018acf3e9fe806
varalakshmi-iOS <varalakshmi1318@gmail.com>
2020-08-15 20:21:35 +0530

    implememnted cards

---
+++ Memorize/Memorize.swift
@@ -0,0 +1,44 @@
+//
+//  Memorize.swift
+//  LoginPage
+//
+//  Created by Vadde Narendra on 8/15/20.
+//  Copyright © 2020 Narendra Vadde. All rights reserved.
+//
+
+import SwiftUI
+
+struct Memorize: View {
+    var body: some View {
+        HStack {
+            ForEach(0..<4) { index in
+                CardView(isFaceUp: true)
+            }
+        }
+        .padding()
+        .font(Font.largeTitle)
+        .foregroundColor(.orange)
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool
+    
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 10).fill(Color.white)
+                RoundedRectangle(cornerRadius: 10).stroke()
+                Text("👻")
+            } else  {
+                RoundedRectangle(cornerRadius: 10).fill()
+            }
+        }
+    }
+}
+
+struct Memorize_Previews: PreviewProvider {
+    static var previews: some View {
+        Memorize()
+    }
+}
